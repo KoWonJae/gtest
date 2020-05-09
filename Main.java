7dcdfecc5a33bbaa62394d1880220ecf2ae37f52
kwj <koj135011@gmail.com>
2020-05-09 10:35:47 +0900

    olyer

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 package OssExma09;
 
 public class Main {
-
+	public static void olyer() {
+		double num = 0;
+		double m = 1;
+		for(int i = 0; i <= 10000000; i++) {
+			num += 1/(m*m);
+			m = m + 1 ;
+		}
+		System.out.println("오일러 곱셈 공식");
+		System.out.println("원주율 :" + Math.sqrt(6 * num));
+	}
 	public static void main(String[] args) {
 		double m = 1;
 		double num = 0;
@@ -11,7 +20,7 @@ public class Main {
 		}
 		System.out.println("라이프니츠 급수");
 		System.out.println("원주율 :" + (4 * num));
-
+		olyer();
 	}
 
 }
